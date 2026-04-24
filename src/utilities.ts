668cd47a65ad0b3c1fb296f02e5c4aabca1d6ba6
Marwanmosalamy <marwanmosalamy89@gmail.com>
2026-04-24 20:02:46 +0300

    fix: handle __Secure cookie name on HTTPS (Vercel)

--- src/utilities.ts
+++ src/utilities.ts
@@ -3,13 +3,16 @@ import { cookies } from "next/headers";
 
 export async function getMyToken() {
   const cookie = await cookies();
-  const myToken = cookie.get("next-auth.session-token")?.value;
+
+  const myToken =
+    cookie.get("__Secure-next-auth.session-token")?.value ??
+    cookie.get("next-auth.session-token")?.value;
+
   const decodedToken = await decode({
     token: myToken,
     secret: process.env.NEXTAUTH_SECRET!,
   });
   const token = decodedToken?.routeToken;
-  console.log("decodedToken", decodedToken);
 
   return token;
 }
